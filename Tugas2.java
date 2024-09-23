3483b6b22ef9037135cf20ff4e399ae48e1c5df4
Rosintan Sitinjak <2381014@unai.edu>
2024-09-23 14:28:05 +0700

    Tugas 2

--- Tugas2.java
+++ Tugas2.java
@@ -5,81 +5,78 @@ public class Tugas2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-
-        System.out.println("Pilihlah bangun datar: ");
-        System.out.println("1. Persegi");
+        System.out.println("Pilihlah Bangun Datar Dibawah Ini: ");
+        System.out.println("\n1. Persegi");
         System.out.println("2. Persegi Panjang");
         System.out.println("3. Lingkaran\n");
 
-        System.out.print("Masukkan Pilihan bangun datar yang ingin anda pilih: ");
+        System.out.print("Masukkan pilihan bangun datar yang ingin anda pilih: ");
         int choose = input.nextInt();
 
 
         switch (choose) {
             case 1:
-                countSquare();
+                System.out.print("Masukkan Sisi Persegi: ");
+                double sisi = input.nextDouble();
+
+                System.out.println("\nLuas Persegi : " + luasPersegi(sisi));
+                System.out.println("Keliling Persegi: " + kelilingPersegi(sisi));
                 break;
+
             case 2:
-                countRectangle();
+                System.out.print("Masukkan Panjang: ");
+                double panjang = input.nextDouble();
+                System.out.print("Masukkan Lebar: ");
+                double lebar = input.nextDouble();
+
+                System.out.println("\nLuas Persegi Panjang: " + luasPersegiPanjang(panjang, lebar));
+                System.out.println("Keliling Persegi Panjang: " + kelilingPersegiPanjang(panjang, lebar));
                 break;
+
             case 3:
-                countCircle();
+                System.out.print("Masukkan Jari-jari Lingkaran: ");
+                double jariJari = input.nextDouble();
+
+                System.out.println("\nLuas Lingkaran: " + luasLingkaran(jariJari));
+                System.out.println("Keliling Lingkaran: " + kelilingLingkaran(jariJari));
                 break;
+
             default:
-                System.out.print("Maaf Pilihan Anda Tidak Ada! Silahkan Memilih Ulang");
+                System.out.print("Maaf Pilihan Anda Tidak Ada! Silahkan Memilih Ulang Bangun Datar");
         }
         input.close();
 
     }
 
-    public static void countSquare() {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan Sisi Persegi: ");
-        double sisi = input.nextDouble();
-
-        double luas = sisi * sisi;
-        double keliling = 4 * sisi;
-
-        System.out.println("\nLuas Persegi : " + luas);
-        System.out.println("Keliling Persegi: " + keliling);
-
-        input.close();
-
+    //method untuk luas persegi
+    public static double luasPersegi(double sisi) {
+        return sisi * sisi;
     }
 
-    public static void countRectangle() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Masukkan Panjang: ");
-        double panjang = input.nextDouble();
-        System.out.print("Masukkan Lebar: ");
-        double lebar = input.nextDouble();
-
-        double luasPersegiPanjang = panjang * lebar;
-        double kelilingPersegiPanjang = 2 * (panjang + lebar);
-
-        System.out.println("\nLuas Persegi Panjang: " + luasPersegiPanjang);
-        System.out.println("Keliling Persegi Panjang: " + kelilingPersegiPanjang);
-
-        input.close();
+    //method untuk luas keliling persegi
+    public static double kelilingPersegi(double sisi) {
+        return 4 * sisi;
     }
 
-    public static void countCircle() {
-        Scanner input = new Scanner(System.in);
+    //method untuk luas persegi panjang
+    public static double luasPersegiPanjang(double panjang, double lebar) {
+        return panjang * lebar;
+    }
 
-        System.out.print("Masukkan Jari-jari Lingkaran: ");
-//        System.out.println("Masukkan Keliling Lingkaran: ");
-        double jariJari = input.nextDouble();
-//        double keliling = input.nextDouble();
+    //method untuk keliling persegi panjang
+    public static double kelilingPersegiPanjang(double panjang, double lebar) {
+        return 2 * (panjang + lebar);
+    }
 
-        double luasLingkaran = Math.PI * jariJari * jariJari;
-        double kelilingLingkaran = 2 * Math.PI * jariJari;
+    //method untuk luas lingkaran
+    public static double luasLingkaran(double jariJari) {
+        return Math.PI * jariJari * jariJari;
+    }
 
-        System.out.println("\nLuas Lingkaran: " + luasLingkaran);
-        System.out.println("Keliling Lingkaran: " + kelilingLingkaran);
-
-        input.close();
+    //method untuk keliling lingkaran
+    public static double kelilingLingkaran(double jariJari) {
+        return 2 * Math.PI * jariJari;
     }
 
 }
